4d6e73c715a850247c8f01a0da1477d575ecaf6c
matvieiev serhii <sermatveev95@gmail.com>
2019-05-21 17:47:53 +0300

    add editor poem name

--- src/app/admin/poems-edit/poems-edit.component.ts
+++ src/app/admin/poems-edit/poems-edit.component.ts
@@ -15,10 +15,16 @@ export class PoemsEditComponent implements OnInit {
   autor: FormGroup;
   poemName: FormGroup;
   poem: FormGroup;
+  poemNameAutor: FormGroup;
+  poemText: FormGroup;
 
   isEditingAutor = false;
   isEditingPoem = false;
   isEditingPoemName = false;
+
+  selectPoemNameAutor;
+  selectAutorData;
+  textAr = '';
 
   pointer;
 
@@ -29,9 +35,23 @@ export class PoemsEditComponent implements OnInit {
       this.apiDataService.getPoems().subscribe((poems) => {
         this.dataPoems = poems;
         localStorage.setItem('poems', JSON.stringify(poems));
-        console.log(this.dataPoems);
       });
     }
+
+    this.poemNameAutor = new FormGroup({
+      poemAutor: new FormControl('')
+    });
+
+    this.poemNameAutor.valueChanges.subscribe((changes) => {
+      this.selectPoemNameAutor = changes.poemAutor;
+      this.selectAutorData = this.dataPoems.filter((el) => {
+        return el.name === this.selectPoemNameAutor;
+      });
+      console.log(this.selectAutorData);
+    });
+    this.poemText = new FormGroup({
+      textArea: new FormControl('')
+    });
   }
 
   editAutor(i) {
@@ -42,7 +62,26 @@ export class PoemsEditComponent implements OnInit {
     this.pointer = i;
   }
 
+  editPoemName(i) {
+    this.poemName = new FormGroup({
+      poemNameFild: new FormControl(this.selectAutorData[0].poems[i].poemName)
+    });
+    this.isEditingPoemName = true;
+    this.pointer = i;
+  }
+
   successEditAutor() {
+    if (this.poemName.value.poemNameFild === this.selectAutorData[0].poems[this.pointer].poemName) {
+      this.isEditingPoemName = false;
+    } else {
+      this.selectAutorData[0].poems[this.pointer].poemName = this.poemName.value.poemNameFild;
+      this.apiDataService.updatePoems(this.dataPoems);
+      localStorage.poems = JSON.stringify(this.dataPoems);
+      this.isEditingPoemName = false;
+    }
+  }
+
+  successEditPoemName() {
     if (this.autor.value.autorFild === this.dataPoems[this.pointer].name) {
       this.isEditingAutor = false;
     } else {
@@ -54,5 +93,11 @@ export class PoemsEditComponent implements OnInit {
 
   cancel() {
     this.isEditingAutor = false;
+    this.isEditingPoemName = false;
+    this.isEditingPoem = false;
+  }
+
+  testTextArea() {
+    console.log(this.poemText.value.textArea);
   }
 }
